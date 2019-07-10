6c553957dffd1fb22eb2be1b1a2154f101ad969a
Piratelle <eratelle@berkeley.edu>
2019-07-10 00:26:30 -0700

    Initial functions.

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+function generateTarget() {
+	  return Math.floor(Math.random() * 10);
+	}
 
+function compareGuesses(humanGuess, computerGuess, targetNumber) {
+	const humanOffBy = Math.abs(targetNumber - humanGuess);
+	const computerOffBy = Math.abs(targetNumber - computerGuess);
+	return (humanOffBy <= computerOffBy);
+}
