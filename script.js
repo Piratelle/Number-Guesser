80c7b5baa03d4b64110c9092745b72606b293e81
Piratelle <eratelle@berkeley.edu>
2019-07-10 00:35:27 -0700

    Streamlined generateTarget using arrow.

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
-function generateTarget() {
-	  return Math.floor(Math.random() * 10);
-	}
+const generateTarget = () => Math.floor(Math.random() * 10);
 
 function compareGuesses(humanGuess, computerGuess, targetNumber) {
 	const humanOffBy = Math.abs(targetNumber - humanGuess);
